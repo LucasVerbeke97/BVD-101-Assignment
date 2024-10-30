5d437ff6fcfc396da252e5a20279f57a76bba059
LucasVerbeke <lucas.verbeke@outlook.com>
2024-10-29 22:55:27 -0400

    working on adding validation to the code

--- src/routes/authorRoutes.ts
+++ src/routes/authorRoutes.ts
@@ -1,9 +1,23 @@
 import express, { Router } from 'express';
+import { body, validationResult } from 'express-validator';
 import { createAuthor, getAuthors, updateAuthor, deleteAuthor } from '../controllers/authorController';
 
 const router: Router = express.Router();
 
-router.post('/', createAuthor);
+const validateAuthor = [
+    body('name')
+        .isString()
+        .withMessage('Name must be a string.')
+        .notEmpty()
+        .withMessage('Name is required.'),
+    body('bio')
+        .isString()
+        .withMessage('Biography must be a string.')
+        .notEmpty()
+        .withMessage('Biography is required.'),
+];
+
+router.post('/', validateAuthor, createAuthor);
 router.get('/', getAuthors);
 router.put('/:id', updateAuthor);
 router.delete('/:id', deleteAuthor);
